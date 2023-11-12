7f16c57391e578ed7b2d1376b7dc03e6b667ec21
renit4 <>
2023-11-12 17:52:30 -0300

    Ej 01

---
+++ ejercicio_complementarios/ejercicio_01.py
@@ -0,0 +1,18 @@
+a = int(input("Ingrese un numero: "))
+b = int(input("Ingrese un segundo numero: "))
+
+res = a + b
+
+print("Suma de los numeros: ", res)
+
+res = a - b
+
+print("resta de los numeros: ", res)
+
+res = a * b
+
+print("multiplicacion de los numeros: ", res)
+
+res = a / b
+
+print("division de los numeros: ", res)
