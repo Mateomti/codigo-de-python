eaaf8805b8e63d034ff99cebf94680833739dac1
renit4 <>
2023-11-12 17:13:37 -0300

    no puden concatenar int's

--- python.py
+++ python.py
@@ -1,7 +1,14 @@
-"""Introduccion a Python"""
 
-print("Hola Mundo!")
 
-print("""hola
-hola de nuevo
-adios""")
+print("Bienvenido!")
+
+nombre = input("Ingrese su nombre: ")
+
+fnac = int(input("\nIngrese su fecha de nacimiento: "))
+
+factual = 2023
+
+edad = factual - fnac
+
+print(" Hola "+nombre+"! Naciste en el "+fnac +
+      ", por lo tanto tenes "+edad+" años!")
