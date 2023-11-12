1e95df1e111ebcf563cfef22e8c9d88b066e1f3d
renit4 <>
2023-11-12 17:37:46 -0300

    microdesafio 01

---
+++ microdesafios/desafio_01.py
@@ -0,0 +1,8 @@
+cadena_01 = " versátil"
+cadena_02 = "Python"
+cadena_03 = " es un lenguaje"
+cadena_04 = " de programacion"
+
+cadena_final = cadena_02 + cadena_03 + cadena_04 + cadena_01
+
+print(cadena_final)
