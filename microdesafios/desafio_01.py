0f20ff0848ce848c4c56ada1c5dc2d9c0cfbb18e
renit4 <>
2023-11-22 21:14:39 -0300

    desafio 01

--- microdesafios/desafio_01.py
+++ microdesafios/desafio_01.py
@@ -1,8 +1,8 @@
-cadena_01 = " versátil"
+cadena_01 = "versátil"
 cadena_02 = "Python"
-cadena_03 = " es un lenguaje"
-cadena_04 = " de programacion"
+cadena_03 = "es un lenguaje"
+cadena_04 = "de programacion"
 
-cadena_final = cadena_02 + cadena_03 + cadena_04 + cadena_01
+cadena_final = f"{cadena_02} {cadena_03} {cadena_04} {cadena_01}"
 
 print(cadena_final)
