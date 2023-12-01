8db08bd119795c54a0fb85b360595e27222912fe
renit4 <>
2023-12-01 14:12:59 -0300

    semana 3+

---
+++ microdesafios/desafio_03.py
@@ -0,0 +1,20 @@
+expresiones = [
+
+    True,
+    True,
+    True,
+    True,
+    True,
+    False
+]
+print(expresiones)
+
+# a = [
+#    not False,
+#    not 3 == 5,
+#    33/3 == 11 and 5 > 2,
+#    True or False,
+#    True * 5 == 2.5*2 or 123 >= 23,
+#    12 > 7 and True < False
+# ]
+# print(a)
